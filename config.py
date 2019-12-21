5da6f622207b29e15288fb1c30f34d60fd1773db
Risto <risto.vesanto1112@gmail.com>
2019-12-21 12:54:45 +0200

    .gitignore changes

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-import env
+import conf_env
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-	SECRET_KEY = env.SECRET_KEY
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or env.SQLALCHEMY_DATABASE_URI
+	SECRET_KEY = conf_env.SECRET_KEY
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or conf_env.SQLALCHEMY_DATABASE_URI
 	SQLALCHEMY_TRACK_MODIFICATIONS = False	
