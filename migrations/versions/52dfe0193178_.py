2792462cc9d8224e159f3e6571f770d64c1100c4
Risto Vesanto <vesanto.dev@gmail.com>
2020-02-16 07:28:21 +0000

    alembic fixed

---
+++ migrations/versions/52dfe0193178_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 52dfe0193178
+Revises: 70b2616237ef, 5136b6c754e2
+Create Date: 2020-02-16 07:26:01.033788
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '52dfe0193178'
+down_revision = ('70b2616237ef', '5136b6c754e2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
