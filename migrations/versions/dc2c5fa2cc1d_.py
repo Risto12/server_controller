32a77052bcf5a7fc4012a951f02f2a82fb377706
Risto Vesanto <vesanto.dev@gmail.com>
2020-02-16 07:03:18 +0000

    alembic fix

---
+++ migrations/versions/dc2c5fa2cc1d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dc2c5fa2cc1d
+Revises: b377fd9a441f
+Create Date: 2019-12-21 10:52:19.781813
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc2c5fa2cc1d'
+down_revision = 'b377fd9a441f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
